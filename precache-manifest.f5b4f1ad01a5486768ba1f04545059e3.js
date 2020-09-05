df7133c762d75322428af99073e2e9fb499b938f
1000ship <cjstjdgur1234@gmail.com>
2020-09-05 20:05:47 +0900

    Updates

---
+++ precache-manifest.f5b4f1ad01a5486768ba1f04545059e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976f203986ba9abfe691342776d06d3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3563f9f2c20079b2987",
+    "url": "/static/js/2.e990ce0f.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.e990ce0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6dae00802d9aee83bb4",
+    "url": "/static/js/main.4d844f17.chunk.js"
+  },
+  {
+    "revision": "060a3e56309e492116de",
+    "url": "/static/js/runtime-main.0ea4973c.js"
+  }
+]);
